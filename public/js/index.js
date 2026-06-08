c6e3a3534a37e2b5a4bdcab4a0a17d0a5c5f17a8
Morgaine Knight <knighmor2020@gmail.com>
2026-06-08 15:12:44 -0400

    added random background image feature -- API key removed

--- public/js/index.js
+++ public/js/index.js
@@ -5,34 +5,54 @@ const elements = {
     author: document.getElementById("author"),
 };
 
-const quotes = [
-    {
-        quote: "All hands! Abandon ship!",
-        author: "Captain Picard",
-    },
+async function getRandomImage() {
+    const client_id = "YOUR_ACCESS_KEY";
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
 
-    {
-        quote: "Doh!",
-        author: "Homer Simpson",
-    },
-
-    {
-        quote: "The Internet is the first thing that humanity has built that humanity doesn't understand, the largest experiment in anarchy that we have ever had.",
-        author: "Eric Schmidt",
+        const imgDiv = document.querySelector(".background-img");
+        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
     }
-]
-
-function loopThroughQuotes() {
-    let quoteIndex = 0;
-    setInterval(() => {
-        if (quoteIndex < quotes.length) {
-            elements.quote.textContent = quotes[quoteIndex].quote;
-            elements.author.textContent = quotes[quoteIndex].author;
-            quoteIndex++;
-        } else {
-            quoteIndex = 0;
-        }
-    }, 3000);
+    catch (error) {
+        console.error(error);
+    }
 }
 
-setTimeout(loopThroughQuotes, 3000);
+getRandomImage();
+
+// #region - quote rotation feature
+// const quotes = [
+//     {
+//         quote: "All hands! Abandon ship!",
+//         author: "Captain Picard",
+//     },
+
+//     {
+//         quote: "Doh!",
+//         author: "Homer Simpson",
+//     },
+
+//     {
+//         quote: "The Internet is the first thing that humanity has built that humanity doesn't understand, the largest experiment in anarchy that we have ever had.",
+//         author: "Eric Schmidt",
+//     }
+// ]
+
+// function loopThroughQuotes() {
+//     let quoteIndex = 0;
+//     setInterval(() => {
+//         if (quoteIndex < quotes.length) {
+//             elements.quote.textContent = quotes[quoteIndex].quote;
+//             elements.author.textContent = quotes[quoteIndex].author;
+//             quoteIndex++;
+//         } else {
+//             quoteIndex = 0;
+//         }
+//     }, 3000);
+// }
+
+// setTimeout(loopThroughQuotes, 3000);
+//#endregion
